7f5c138ed7b95c9fe33a85e4f238509d8ab60a81
didier <didier@d-defrance.fr>
2021-02-17 13:34:51 +0100

    mercredi 13h34

--- my-app/src/app/login2/login2.component.ts
+++ my-app/src/app/login2/login2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Login } from '../common/data/login';
 import { LoginResponse } from '../common/data/loginResponse';
 import { LoginService } from '../common/service/login.service';
 
@@ -31,8 +32,12 @@ export class Login2Component implements OnInit {
 
   public onLogin(){
     this.message = "donnees saisies = " + JSON.stringify(this.myForm.value);
+    let login = new Login();
+    //Object.assign(targetObj,sourceObj) 
+    //pour copier valeurs des propriétés de sourceObj vers targetObj
+    Object.assign(login,this.myForm.value); 
     console.log(this.message);
-    this._loginService.postLogin(this.myForm.value).subscribe(
+    this._loginService.postLogin(login).subscribe(
       {
         next: (loginResponse : LoginResponse)=>{ this.gererLoginResponse(loginResponse); },
         error: (err) => { console.log("error:"+err)}
